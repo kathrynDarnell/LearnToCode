f26f623a47d81471c9fb3bd6fe49e3ff59dd5496
kathrynDarnell <kathryn.n.darnell@gmail.com>
2016-03-15 14:51:11 -0400

    chat

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 /*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
+var express = require('express'),
+	cfenv = require('cfenv'),
+	app = express(),
+	Cloudant = require('cloudant'),
+	appEnv = cfenv.getAppEnv(),
+	cloudant = Cloudant("https://78cfae91-2492-4226-98bc-a10cfd059aa0-bluemix:b413b028d3c69546d9e15a92ad2ecf8e72ad7127d85bff83d23ac3bb1d19c28b@78cfae91-2492-4226-98bc-a10cfd059aa0-bluemix.cloudant.com"),
+	data = cloudant.use('chat'),
+	feed = data.follow({since: "now", include_docs: true}),
+	bodyParser = require("body-parser");
+	
+	
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
+var io = require('socket.io').listen(app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
+}));
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'You just joined the chat!' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
+feed.on('change', function (change) {
+	io.sockets.emit('message', {message: change.doc['user'] + ": " + change.doc['message']});
+
+})
+feed.follow();
+
+
+app.post("/sendAndSave", function(req,res,next){
+	var username = req.body.username;
+	var message = req.body.message;
+	var date = new Date();
+	data.insert({ "user": username, "message": message, 'time': date});
+});
+
+
+app.get("/upToDateMessages", function(req,res,next){
+	data.find({selector:
+		{"time": {"$gt": null}},
+		"sort": [{"time": "asc"}]
+        }, function(er, result) {
+			res.send(result);		
+	});
+	
 });
---
+++ public/chat.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+ 
+	$.get("/upToDateMessages", function (messages){
+			var formattedMessages = "";
+			for (i = 0; i < messages.docs.length; i++) { 
+				formattedMessages += "" + messages.docs[i]["user"] + ":" + messages.docs[i]["message"] + "<br>";
+			}
+			document.getElementById("oldMessages").innerHTML = formattedMessages;
+		}	
+	)
+    var messages = [];
+    var socket = io.connect('http://localhost:6004');
+    var field = document.getElementById("field");
+	var username = document.getElementById("username");
+    var sendButton = document.getElementById("send");
+    var newMessages = document.getElementById("newMessages");
+ 
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            newMessages.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+ 
+    sendButton.onclick = function() {
+        var text = field.value;
+		var user = username.value;
+		$.post("/sendAndSave", { "username": user,"message": text });
+		field.value = "";
+    };
+ 
+}
